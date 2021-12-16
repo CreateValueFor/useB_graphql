ea79fe7d3e008bc649cf1cd17b026be6c3f35a77
이민기 <mingi@imingiui-MacBookPro.local>
2021-12-16 17:26:01 +0900

    Construct graphql resolvers #3

--- src/controller/invocie.ts
+++ src/controller/invocie.ts
@@ -1,22 +1,4 @@
-import { Invoice } from "../models/invoice.model";
 
-const dummyInvoices = () => {
-    return [
-        {
-            id: 1,
-            userid:"woghks08@gmail.com",
-            apidate:"202111",
-            ocr_total: 312,
-            ocr_invalid:12,
-            ocr_month_calls:5000,
-            ocr_month_fee : 300000
-        }
-    ]
-}
 
-const getItems = async () => {
-  const invoices = await Invoice.findAll();
-  return invoices;
-};
 
-export default { getItems };
+
