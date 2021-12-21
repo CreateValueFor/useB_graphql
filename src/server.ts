e045323131066475a9b507c46d5ca3343953d9cd
이민기 <mingi@imingiui-MacBookPro.local>
2021-12-21 19:25:42 +0900

    Chane sequelize hardcoding to env #21

--- src/server.ts
+++ src/server.ts
@@ -5,22 +5,31 @@ import { createServer } from 'http'
 import compression from 'compression'
 import cors from 'cors'
 import { Sequelize } from 'sequelize-typescript'
-
+import dotenv from 'dotenv'
 import authRouter from './router/auth'
 
+dotenv.config()
 // 환경변수 세팅
 // TODO mysql .env로 분리 필요
-require('dotenv').config()
+
+console.log(process.env.DB_DBNAME)
+
+// DB_USERNAME = 'root'
+// DB_PASSWORD = 'mk159753'
+// DB_DBNAME = 'dashboard'
+// DB_HOST = 'localhost'
+// DB_PORT = 3306
 
 import schema from './schema'
+import { Dialect } from 'sequelize/types'
 // sequelize basic setting
 const sequelize = new Sequelize({
-  database: 'dashboard',
-  dialect: 'mysql',
-  host: '127.0.0.1',
-  username: 'root',
-  password: 'mk159753',
-  port: 3306,
+  database: process.env.DB_DBNAME,
+  dialect: process.env.DB_DIALECT as Dialect,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT) as number,
   define: {
     timestamps: false,
   },
@@ -47,10 +56,9 @@ app.use('*', cors())
 // compress dist file to run efficiently
 app.use(compression())
 // apply graphql
+app.use('/auth', authRouter)
 server.applyMiddleware({ app, path: '/graphql' })
 
 const httpServer = createServer(app)
-
-app.use('/auth', authRouter)
 
 httpServer.listen({ port: 80 }, (): void => console.log('server start'))
